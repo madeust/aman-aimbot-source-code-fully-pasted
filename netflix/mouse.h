b2cc011b9633b2a6429ecedff505ec5e1253b61f
Madeust <86959245+madeust@users.noreply.github.com>
2022-08-19 21:52:15 +0300

    Delete netflix directory

--- netflix/mouse.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef int(*pDD_btn)(int btn);
-typedef int(*pDD_movR)(int dx, int dy);
-typedef int(*pDD_mov)(int dx, int dy);
-
-inline pDD_btn mouse_button;
-inline pDD_movR mouse_move_rel;
-inline pDD_mov mouse_move_abs;
